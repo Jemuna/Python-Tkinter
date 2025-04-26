476bf6ddb254505e5598207293787e2bb2ab518e
JEMUNA U <152510293+Jemuna@users.noreply.github.com>
2025-04-26 21:54:02 +0530

    Update Messagebox.py

--- MessageBox/Messagebox.py
+++ MessageBox/Messagebox.py
@@ -4,13 +4,8 @@ from tkinter import messagebox
 def show_message():
     messagebox.showinfo("Greeting", "Hello, welcome to Tkinter!")
 
-# Create the main window
 root = tk.Tk()
 root.title("Message Box Example")
-
-# Create a button to show the message
 button = tk.Button(root, text="Click Me!", command=show_message)
 button.pack(pady=20)
-
-# Start the GUI loop
 root.mainloop()
